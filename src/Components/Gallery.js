3f18bb4cb555305387b4835cca05d7c830fb9e24
Kapable <tech@kapable.club>
2022-08-14 19:22:59 +0530

    init

---
+++ src/Components/Gallery.js
@@ -0,0 +1,23 @@
+export const Images = [
+    {
+        original: 'https://res.cloudinary.com/dmlfy0hjh/image/upload/v1660366677/Pranav%20RaY/pernav_stg4da.jpg',
+        thumbnail: 'https://res.cloudinary.com/dmlfy0hjh/image/upload/v1660366677/Pranav%20RaY/pernav_stg4da.jpg'
+    },
+    {
+        original: 'https://res.cloudinary.com/dmlfy0hjh/image/upload/v1660483842/Pranav%20RaY/WhatsApp_Image_2022-06-30_at_1.08.11_PM_qpzdfm.jpg',
+        thumbnail: 'https://res.cloudinary.com/dmlfy0hjh/image/upload/v1660483842/Pranav%20RaY/WhatsApp_Image_2022-06-30_at_1.08.11_PM_qpzdfm.jpg'
+    },
+    {
+        original: 'https://res.cloudinary.com/dmlfy0hjh/image/upload/v1660484369/Pranav%20RaY/WhatsApp_Image_2022-06-26_at_8.47.12_PM_gxwhcj.jpg',
+        thumbnail: 'https://res.cloudinary.com/dmlfy0hjh/image/upload/v1660484369/Pranav%20RaY/WhatsApp_Image_2022-06-26_at_8.47.12_PM_gxwhcj.jpg'
+    },
+    {
+        original: 'https://res.cloudinary.com/dmlfy0hjh/image/upload/v1660484592/Pranav%20RaY/WhatsApp_Image_2022-07-16_at_12.12.24_AM_ugjjje.jpg',
+        thumbnail: 'https://res.cloudinary.com/dmlfy0hjh/image/upload/v1660484592/Pranav%20RaY/WhatsApp_Image_2022-07-16_at_12.12.24_AM_ugjjje.jpg'
+    },
+    {
+        original: 'https://res.cloudinary.com/dmlfy0hjh/image/upload/v1660484672/Pranav%20RaY/WhatsApp_Image_2022-07-24_at_12.05.56_PM_mmazbd.jpg',
+        thumbnail: 'https://res.cloudinary.com/dmlfy0hjh/image/upload/v1660484672/Pranav%20RaY/WhatsApp_Image_2022-07-24_at_12.05.56_PM_mmazbd.jpg'
+    }
+]
+   
